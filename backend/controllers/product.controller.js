9779536126370d3a83cb2ff7e6ad9511a48abc6a
sharafath ali <sharafathaliofficial@gmail.com>
2024-11-17 20:54:37 +0530

    added two more product routes

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -100,3 +100,39 @@ export const getRecommendedProducts = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
+
+export const getproductsbycategory = async (req, res) => {
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category });
+    res.status(200).json({ products })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const toggleFeaturedproduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    product.isFeatured = !product.isFeatured;
+    const updatedProduct = await product.save();
+    await updatefeaturedproductcache()
+
+    res.status(200).json({ product })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+async function updatefeaturedproductcache() {
+  try {
+    const featuredProducts = await Product.find({ isFeatured: true }).lean();
+    await redis.set('featured_products', JSON.stringify(featuredProducts));
+  } catch (error) {
+    console.log(error)
+  }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getallproducts, getfeaturedproducts, createproduct, deleteproduct, getRecommendedProducts } from "../controllers/product.controller.js";
+import { getallproducts, getfeaturedproducts, createproduct, deleteproduct, getRecommendedProducts, getproductsbycategory, toggleFeaturedproduct } from "../controllers/product.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { adminRoute } from "../middleware/auth.middleware.js";
 import { getRounds } from "bcrypt";
@@ -9,6 +9,8 @@ const router = express.Router();
 router.get("/", protectRoute, adminRoute, getallproducts)
 router.get("/featured", getfeaturedproducts)
 router.post("/create", protectRoute, adminRoute, createproduct)
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedproduct)
 router.post("/delete/:id", protectRoute, adminRoute, deleteproduct)
 router.get("/recommend", getRecommendedProducts)
+router.get("/category/:category", getproductsbycategory)
 export default router
