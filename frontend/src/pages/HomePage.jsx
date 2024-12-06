969b2a2194841651f225ffcd85f8b85d8838a0d3
sharafath ali <sharafathaliofficial@gmail.com>
2024-12-06 22:26:01 +0530

    added pants image

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const categories = [
   { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
   { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
   { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
-  { href: "/pants", name: "Pants", imageUrl: "/pants.jpg" },
+  { href: "/pants", name: "Pants", imageUrl: "/pant.jpg" },
 ];
 
 const HomePage = () => {
